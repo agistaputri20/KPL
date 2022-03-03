e24f54266ecdc664fa3af5e45b6532d0b314458c
agistaputri20 <agistaputri@student.telkomuniversity.ac.id>
2022-03-03 13:28:02 +0700

    TP2

---
+++ TP2/TP2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string masukan = input.Text;
+            string keluaran = "Halo " + masukan;
+            output.Text = keluaran;
+        }
+
+        private void output_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
